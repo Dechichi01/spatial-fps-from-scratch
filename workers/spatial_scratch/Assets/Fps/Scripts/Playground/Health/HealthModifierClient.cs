ea1f3cec7b20ec8446bf4e098985dbf1873607e6
Dechichi <gabrieldechichi@gmail.com>
2019-04-18 13:54:51 -0300

    add health client and server example, with Monobehaviour workflow

---
+++ workers/spatial_scratch/Assets/Fps/Scripts/Playground/Health/HealthModifierClient.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using Improbable.Gdk.Health;
+using Improbable.Gdk.Subscriptions;
+using Improbable.Gdk.Standardtypes;
+
+namespace Fps.Playground
+{
+    public class HealthModifierClient : MonoBehaviour
+    {
+        [Require] HealthComponentCommandSender healthCommandSender;
+        [Require] HealthComponentReader healthReader;
+
+        LinkedEntityComponent localPlayerLink;
+        LinkedEntityComponent LocalPlayerLink
+        {
+            get { return localPlayerLink ?? (localPlayerLink = GetComponent<LinkedEntityComponent>()); }
+        }
+
+        private void OnEnable()
+        {
+            if (healthReader != null)
+            {
+                healthReader.OnHealthModifiedEvent += OnHealthModified;
+            }
+        }
+
+        private void OnHealthModified(HealthModifiedInfo obj)
+        {
+            Debug.Log(string.Format(
+                "Received health modified event: Before {0}, After {1}", obj.HealthBefore, obj.HealthAfter));
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0) && LocalPlayerLink)
+            {
+                healthCommandSender.SendModifyHealthCommand(BuildModifyHealthCommand(10), OnModifyHealthResponse);
+            }
+            else if (Input.GetMouseButtonDown(1) && LocalPlayerLink)
+            {
+                healthCommandSender.SendModifyHealthCommand(BuildModifyHealthCommand(-10), OnModifyHealthResponse);
+            }
+        }
+
+        private HealthComponent.ModifyHealth.Request BuildModifyHealthCommand(float delta)
+        {
+            var zeroStandardType = new IntAbsolute();
+            var healthModifier = new HealthModifier(0, delta, zeroStandardType, zeroStandardType);
+
+            return new HealthComponent.ModifyHealth.Request(
+                LocalPlayerLink.EntityId,
+                healthModifier);
+        }
+
+        private void OnModifyHealthResponse(HealthComponent.ModifyHealth.ReceivedResponse response)
+        {
+            Debug.Log("Received response: " + response.StatusCode + ", " + response.Message);
+        }
+    }
+}
+
---
+++ workers/spatial_scratch/Assets/Fps/Scripts/Playground/Health/HealthModifierServer.cs
@@ -0,0 +1,40 @@
+﻿using Improbable.Gdk.Subscriptions;
+using Improbable.Gdk.Health;
+using UnityEngine;
+
+namespace Fps.Playground
+{
+    public class HealthModifierServer : MonoBehaviour
+    {
+        [Require] HealthComponentCommandReceiver healthCommandReceiver;
+        [Require] HealthComponentWriter healthWriter;
+
+        private void OnEnable()
+        {
+            healthCommandReceiver.OnModifyHealthRequestReceived += OnModifyHealthRequestReceived;
+        }
+
+        private void OnModifyHealthRequestReceived(HealthComponent.ModifyHealth.ReceivedRequest obj)
+        {
+            Debug.Log("Request received from: " + obj.CallerWorkerId);
+
+            HandleHealthModifier(obj.Payload);
+
+            var response = new HealthComponent.ModifyHealth.Response(obj.RequestId, new Improbable.Common.Empty());
+            healthCommandReceiver.SendModifyHealthResponse(response);
+        }
+
+        private void HandleHealthModifier(HealthModifier modifyReq)
+        {
+            var oldHealth = healthWriter.Data.Health;
+            var newHealth = Mathf.Clamp(oldHealth + modifyReq.Amount, 0, healthWriter.Data.MaxHealth);
+            var died = newHealth <= 0;
+
+            healthWriter.SendUpdate(new HealthComponent.Update() { Health = newHealth });
+
+            var healthModifiedInfo = new HealthModifiedInfo(modifyReq, oldHealth, newHealth, died);
+            healthWriter.SendHealthModifiedEvent(healthModifiedInfo);
+        }
+    }
+
+}
